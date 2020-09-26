b417f03ef5dfceb8bc90af7927ebce24667fe20f
João Neto <joaocshunderlick26@gmail.com>
2020-09-26 09:50:46 -0300

    Ajuste na rota

--- server.js
+++ server.js
@@ -2,16 +2,17 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-
+// view engine setup
+app.set("views", path.join(__dirname, "src"));
+app.set("view engine", "ejs");
 app.use("/public", express.static(path.join(__dirname, 'public')));
 
 app.get('/em-construcao', function (req, res) {
     res.sendFile('index.html', {root: __dirname })
   });
 
-  app.get('/', function (req, res) {
-    res.sendFile('index.html', {root: __dirname })
-  });
-
+app.get('/', function (req, res) {
+  res.render('index')
+});
 
 app.listen(3000);
